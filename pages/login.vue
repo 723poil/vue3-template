33d5603fafdab2b690e5032839ad727b5b89f28c
723poil <leetkdguq73@naver.com>
2024-10-31 23:45:00 +0900

    2024-10-31 로그인 페이지의 카드 컴포넌트 클라이언트에서 렌더링 되도록 수정

--- pages/login.vue
+++ pages/login.vue
@@ -5,32 +5,34 @@
         <t-col class="flex flex-col items-center justify-center align-middle">
           <t-row>
             <t-col>
-              <t-card style="width: 400px; height: 450px" title-class="flex justify-center align-middle">
-                <template #title>
-                  <div class="h-48 flex flex-col items-center justify-center align-middle"> 로그인 </div>
-                </template>
-                <template #content>
-                  <t-row>
-                    <t-col class="pt-3">
-                      <t-text-field
-                        placeholder="아이디"
-                        :model-value="id"
-                        @update:model-value="updateId"
-                      ></t-text-field>
-                    </t-col>
-                    <t-col class="pt-3">
-                      <t-text-field
-                        placeholder="비밀번호"
-                        :model-value="password"
-                        @update:model-value="updatePassword"
-                      ></t-text-field>
-                    </t-col>
-                    <t-col class="pt-3">
-                      <t-button block @click="login">로그인</t-button>
-                    </t-col>
-                  </t-row>
-                </template>
-              </t-card>
+              <client-only>
+                <t-card style="width: 400px; height: 450px" title-class="flex justify-center align-middle">
+                  <template #title>
+                    <div class="h-48 flex flex-col items-center justify-center align-middle"> 로그인 </div>
+                  </template>
+                  <template #content>
+                    <t-row>
+                      <t-col class="pt-3">
+                        <t-text-field
+                          placeholder="아이디"
+                          :model-value="id"
+                          @update:model-value="updateId"
+                        ></t-text-field>
+                      </t-col>
+                      <t-col class="pt-3">
+                        <t-text-field
+                          placeholder="비밀번호"
+                          :model-value="password"
+                          @update:model-value="updatePassword"
+                        ></t-text-field>
+                      </t-col>
+                      <t-col class="pt-3">
+                        <t-button block @click="login">로그인</t-button>
+                      </t-col>
+                    </t-row>
+                  </template>
+                </t-card>
+              </client-only>
             </t-col>
           </t-row>
         </t-col>
